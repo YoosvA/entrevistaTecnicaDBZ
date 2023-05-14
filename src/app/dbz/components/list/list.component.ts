9cd3aa45bd59363474ae62b448e78446425f92f5
YoosvA <Computadoraz63@gmail.com>
2023-05-14 17:53:47 -0600

    DBZEntrevista

---
+++ src/app/dbz/components/list/list.component.ts
@@ -0,0 +1,15 @@
+import { Component, Input } from '@angular/core';
+import { Personaje } from '../../interfaces/personajes.interface';
+
+@Component({
+  selector: 'app-dbz-list',
+  templateUrl: './list.component.html',
+  styleUrls: ['./list.component.css']
+})
+export class ListComponent {
+  @Input()
+  public personajeList : Personaje[] = [{
+    nombre : "Vegeta", 
+    poder : 20000
+  }];
+}
---
+++ src/app/dbz/pages/main-page/main-page.component.ts
@@ -0,0 +1,21 @@
+import { Component } from '@angular/core';
+import { Personaje } from '../../interfaces/personajes.interface';
+
+@Component({
+  selector: 'app-dbz-main-page',
+  templateUrl: './main-page.component.html',
+  styleUrls: ['./main-page.component.css']
+})
+export class MainPageComponent {
+  public personajes : Personaje[] = [
+    {nombre : "Goku", poder : 20000}
+  ];
+  
+
+  public obtenNuevoPersonaje(personaje : Personaje) : void{
+    
+    // console.log(personaje);
+    // agrega al arreglo personajes un nuevo personaje obtenido
+    this.personajes.push(personaje);
+  }
+}
